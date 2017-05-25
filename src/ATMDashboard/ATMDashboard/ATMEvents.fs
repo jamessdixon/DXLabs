15db00029872c3706f299a622ead7f8cc0003fd0
Jamie Dixon <jamessdixon@gmail.com>
2017-05-25 09:08:35 -0400

    got Aggregator to compile

--- src/ATMDashboard/ATMDashboard/ATMEvents.fs
+++ src/ATMDashboard/ATMDashboard/ATMEvents.fs
@@ -16,41 +16,35 @@ type ATMEvent = {
 
 type ATMEventAggregator() =
     let events = new List<ATMEvent>()
-    let LogEvent(event) =
+    member this.LogEvent(event) =
         match events.Count < 1024 with
         | true -> events.Add(event)
         | false -> ()
-    let GetLoggedEvents() =
+    member this.GetLoggedEvents() =
         let eventArray = events.ToArray()
         events.Clear()
         eventArray
 
-type SimpleEventProcessor() =
+type SimpleEventProcessor(aggregator:ATMEventAggregator) =
     let checkpointStopWatch = new Stopwatch()
     interface IEventProcessor with
         member this.CloseAsync(context, reason) = 
             Debug.WriteLine("Processor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason)
             match reason with 
-            | CloseReason.Shutdown -> await context.CheckpointAsync()
-            | _ -> ()
+            | CloseReason.Shutdown -> context.CheckpointAsync() 
+            | _ -> Task.CompletedTask   
         member this.OpenAsync(context) =
             Debug.WriteLine("SimpleEventProcessor initialized.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.LeaseOffset)
             checkpointStopWatch.Start()
-            return Task.FromResult<object>(null)
+            Task.CompletedTask
         member this.ProcessEventsAsync(context, messages) =
             messages
             |> Seq.map(fun ed -> Encoding.UTF8.GetString(ed.GetBytes()))
-            |> Seq.map(fun b -> Debug.WriteLine(string.Format("Message received.  Partition: '{0}', Data: '{1}'", context.Lease.PartitionId, data)); b)
+            |> Seq.map(fun b -> Debug.WriteLine(String.Format("Message received.  Partition: '{0}', Data: '{1}'", context.Lease.PartitionId, data)); b)
             |> Seq.map(fun b -> JsonConvert.DeserializeObject<ATMEvent>(b))
-            |> Seq.map(fun e -> ATMEventAggregator.LogEvent(e))
+            |> Seq.iter(fun e -> aggregator.LogEvent(e))
             
             match checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(5.0) with
-            | true -> 
-                checkpointStopWatch.Restart()
-                await context.CheckpointAsync()
-            | false ->
-                ()
-
-
-
-                
+            | true -> checkpointStopWatch.Restart()
+                      context.CheckpointAsync()
+            | false -> Task.CompletedTask                 
